02b647ee476a2cbbbd166b25d4829055d6bf09ba
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-12-21 21:48:26 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-export default function Index() {
+import { JSX } from 'solid-js';
+import { isServer } from 'solid-js/web';
+
+interface IndexProps {
+  data: string;
+}
+
+function Index(props: IndexProps): JSX.Element {
   return (
-    <h1>Hello World</h1>
+    <h1>{props.data}</h1>
   );
 }
+
+if (isServer) {
+  const sleep = (ms: number) => new Promise((resolve) => {
+    setTimeout(resolve, ms, true);
+  });
+  Index.getData = async (request: Request) => {
+    await sleep(100);
+    return 'Hello World';
+  };
+}
+
+export default Index;
