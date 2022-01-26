026cdd90f5e8547039895e5e7ae80e71b0c1d2fb
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-01-26 18:13:00 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 /* @jsxImportSource solid-js */
-import { JSX, Suspense } from 'solid-js';
+
+'use solid-sfc';
+
+import { Suspense } from 'solid-js';
 import { Link, AppProps } from 'rigidity';
 import styles from '../styles/main.css?url';
 
-export default function App(props: AppProps): JSX.Element {
-  return (
-    <>
-      <Link rel="stylesheet" href={styles} />
-      <div class="flex flex-col p-4 w-full min-h-screen bg-gradient-to-tr from-slate-500 to-gray-700">
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <props.Component />
-        </Suspense>
-      </div>
-    </>
-  );
-}
+const props = $props<AppProps>();
+
+export default (
+  <>
+    <Link rel="stylesheet" href={styles} />
+    <div class="flex flex-col p-4 w-full min-h-screen bg-gradient-to-tr from-slate-500 to-gray-700">
+      <Suspense fallback={<h1>Loading...</h1>}>
+        {props.children}
+      </Suspense>
+    </div>
+  </>
+);
