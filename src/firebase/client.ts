adf9848276a89e4c5c9b05769e02bcbc43484950
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-01-26 18:12:06 +0800

    Rework firebase

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -2,25 +2,17 @@ import {
   initializeApp,
 } from 'firebase/app';
 import {
-  getFirestore,
   collection,
-  doc,
-  getDocs,
   CollectionReference,
+  doc,
   DocumentReference,
-  QuerySnapshot,
+  getFirestore,
 } from 'firebase/firestore';
 import {
   getAuth,
   signInAnonymously,
   UserCredential,
 } from 'firebase/auth';
-import {
-  Message,
-  MessageReact,
-  MessageComment,
-  MessageCommentReact,
-} from '../types/collections';
 
 const FIREBASE_CLIENT_APP = initializeApp({
   apiKey: process.env.FIREBASE_CLIENT_API_KEY,
@@ -34,70 +26,29 @@ const FIREBASE_CLIENT_APP = initializeApp({
 const FIREBASE_FIRESTORE = getFirestore(FIREBASE_CLIENT_APP);
 const FIREBASE_AUTH = getAuth(FIREBASE_CLIENT_APP);
 
-export function getDiscussions(): CollectionReference {
-  return collection(FIREBASE_FIRESTORE, 'discussions');
-}
-
-export function getDiscussion(
-  start: string,
-  end: string,
-): DocumentReference {
-  return doc(getDiscussions(), `${start}-${end}`);
-}
-
-export function getMessages(
-  start: string,
-  end: string,
-): CollectionReference<Message> {
-  return collection(getDiscussion(start, end), 'messages') as CollectionReference<Message>;
-}
-
-export function getMessage(
-  start: string,
-  end: string,
-  id: string,
-): DocumentReference<Message> {
-  return doc(getMessages(start, end), id);
-}
-
-export function getMessageReacts(
-  start: string,
-  end: string,
-  id: string,
-): CollectionReference<MessageReact> {
-  return collection(getMessage(start, end, id), 'reacts') as CollectionReference<MessageReact>;
-}
-
-export function getMessageComments(
-  start: string,
-  end: string,
-  id: string,
-): CollectionReference<MessageComment> {
-  return collection(getMessage(start, end, id), 'comments') as CollectionReference<MessageComment>;
-}
-
-export function getMessageComment(
-  start: string,
-  end: string,
-  id: string,
-  commentId: string,
-): DocumentReference<MessageComment> {
-  return doc<MessageComment>(getMessageComments(start, end, id), commentId);
-}
-
-export function getMessageCommentReacts(
-  start: string,
-  end: string,
-  id: string,
-  commentId: string,
-): CollectionReference<MessageCommentReact> {
-  return collection(getMessageComment(start, end, id, commentId), 'reacts') as CollectionReference<MessageCommentReact>;
-}
-
-export async function queryMessages(start: string, end: string): Promise<QuerySnapshot<Message>> {
-  return getDocs(getMessages(start, end));
-}
-
 export async function signIn(): Promise<UserCredential> {
   return signInAnonymously(FIREBASE_AUTH);
+}
+
+export function getMessagesCollection(): CollectionReference {
+  return collection(FIREBASE_FIRESTORE, 'messages');
+}
+
+export function getMessageDoc(messageId: string): DocumentReference {
+  return doc(getMessagesCollection(), messageId);
+}
+
+export function getMessageRepliesCollection(messageId: string): CollectionReference {
+  return collection(getMessageDoc(messageId), 'replies');
+}
+
+export function getMessageReplyDoc(messageId: string, replyId: string): DocumentReference {
+  return doc(getMessageRepliesCollection(messageId), replyId);
+}
+
+export function getMessageReplyReactsCollection(
+  messageId: string,
+  replyId: string,
+): CollectionReference {
+  return collection(getMessageReplyDoc(messageId, replyId), 'reacts');
 }
--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -8,9 +8,10 @@ const CREDENTIALS: AppOptions = {
   credential: credential.cert({
     projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(new RegExp('\\\\n', 'g'), '\n'),
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
   serviceAccountId: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
 };
 
 const FIREBASE_ADMIN = initializeApp(CREDENTIALS);
+
